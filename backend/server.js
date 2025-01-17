b9984a34f41e564a8962ea1c80373840bd8f65e2
Gerlyn Tan <gerlyntan07@gmail.com>
2025-01-17 23:13:22 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -37,8 +37,10 @@ dotenv.config();
 const app = express();
 
 const corsConfig = {
-    origin: "https://group4-enrollment-system-client.vercel.app",
-    credential: true,
+    origin: ["https://group4-enrollment-system-client.vercel.app",
+        "http://localhost:5173"
+    ],
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 app.options("", cors(corsConfig));
