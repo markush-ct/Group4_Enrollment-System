cb896523bfa6a323ff73a4a7fe8b294df22cbbf5
Gerlyn Tan <gerlyntan07@gmail.com>
2025-01-17 21:26:47 +0800

    fixed cors in server.js

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173",
+        "https://group4-enrollment-system-client.vercel.app" 
+    ],
     methods: ["POST", "GET"],
     credentials: true
 }));
