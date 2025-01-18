b8fbccf57679e050ef8ef9138737b7499cfccbff
Gerlyn Tan <gerlyntan07@gmail.com>
2025-01-18 19:09:03 +0800

    test

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,7 @@ dotenv.config();
 const app = express();
 
 const corsConfig = {
-    origin: ["https://group4-enrollment-system-client.vercel.app",
-        "http://localhost:5173"
-    ],
+    origin: ['http://localhost:5173', 'https://group4-enrollment-system-server.vercel.app'], // Replace with your frontend's production URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
