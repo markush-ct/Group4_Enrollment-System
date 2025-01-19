dce3317e57d554d8039b1efe157237990abd5727
Gerlyn Tan <gerlyntan07@gmail.com>
2025-01-20 00:22:19 +0800

    session cookie secure to true

--- backend/routes/socOfficerProgram.js
+++ backend/routes/socOfficerProgram.js
@@ -20,11 +20,18 @@ router.get('/socOfficerProgram', (req, res) => {
     db.query(sql, req.session.email, (err, result) => {
         if (err) {
             console.error('Error getting society officer:', err.message);
+            if (err.fatal) {
+                db.end(); // Close the connection
+                db.connect(); // Reconnect if necessary
+            }
             return res.json('Error getting society officer.');
         } else if(result.length > 0) {
             return res.json({program: result[0].ProgramID});
+        } else {
+            return res.json('No society officer found.');
         }
     });
 });
+
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: false,
+        secure: true,
         maxAge: 1000 * 60 * 60 * 24
     }
 }));
